b52f0ab35c7c66f0b091e7e61fb90ff3ae4f5b01
deepa_kashyap <deepa494671@gmail.com>
2025-08-29 11:56:00 +0530

    .

--- Patterns2.java
+++ Patterns2.java
@@ -15,52 +15,132 @@ public class Patterns2 {
         }
     }
 
-
-    //inverted half pyramid 
-    public static void invertdHalfPyramid(int n ){
-        for (int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
+    // inverted half pyramid
+    public static void invertdHalfPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print("*"); 
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 
+    // num trangle
 
-    //num trangle
-
-    public static void numTriangle(int n ){
-        for (int i=1; i<=n; i++){
-            for (int j = 1; j<=n-i+1; j++){
+    public static void numTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
     }
 
-
-
-    public static void floyds_Triangle(int n ){
+    public static void floyds_Triangle(int n) {
         int counter = 1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(counter+" ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
                 counter++;
             }
             System.out.println();
         }
     }
+
+    public static void zero_one_triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterfly(int n) {
+        // 1st half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // 2nd half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void solidRhoumbus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamon(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+         for (int i = n; i>=1; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         // int totRows= sc.nextInt();
         // int totCols = sc.nextInt();
-        int n = sc.nextInt();
+        // int n = sc.nextInt();
         // hollowRectangle(totRows, totCols);
         // invertdHalfPyramid(n);
         // numTriangle(n);
-        floyds_Triangle(n);
+        // floyds_Triangle(n);
+        // zero_one_triangle(5);
+        // butterfly(5);
+        // solidRhoumbus(7);
+        diamon(3);
     }
 }
